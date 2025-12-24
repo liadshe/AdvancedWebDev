90f8ed4a305515706409568966a5a70fa8e7db62
liadshe <liadshe1@gmail.com>
2025-12-24 16:18:34 +0200

    add tests to authTest

--- dist/src/tests/auth.test.js
+++ dist/src/tests/auth.test.js
@@ -15,35 +15,54 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const supertest_1 = __importDefault(require("supertest"));
 const index_1 = __importDefault(require("../index"));
 const userModel_1 = __importDefault(require("../model/userModel"));
+const moviesModel_1 = __importDefault(require("../model/moviesModel"));
 let app;
+const user = { email: "test@example.com", password: "testpassword", _id: "", token: "" };
+const movie1 = { title: "Test Movie", year: 2025 };
+const movie2 = { title: "Another Test Movie", year: 2024 };
 beforeAll(() => __awaiter(void 0, void 0, void 0, function* () {
     console.log("Befroe All Tests");
     app = yield (0, index_1.default)();
     yield userModel_1.default.deleteMany();
+    yield moviesModel_1.default.deleteMany();
 }));
 afterAll(done => {
     done();
 });
 describe('Auth API', () => {
+    test("Access restricted url denied", () => __awaiter(void 0, void 0, void 0, function* () {
+        const response = yield (0, supertest_1.default)(app).post('/movie').send(movie1);
+        expect(response.statusCode).toBe(401);
+    }));
     test('Register Test', () => __awaiter(void 0, void 0, void 0, function* () {
-        const resoponse = yield (0, supertest_1.default)(app).post('/auth/register').send({
-            email: "test@example.com",
-            password: "testpassword"
-        });
+        const resoponse = yield (0, supertest_1.default)(app).post('/auth/register').send(user);
+        user._id = resoponse.body._id;
         expect(resoponse.body).toHaveProperty("token");
         expect(resoponse.statusCode).toBe(201);
     }));
     test('Login Test', () => __awaiter(void 0, void 0, void 0, function* () {
-        const response = yield (0, supertest_1.default)(app).post('/auth/login').send({
-            email: "test@example.com",
-            password: "testpassword"
-        });
-        if (response.statusCode !== 200) {
-            console.log("Error Body:", response.body);
-            console.log("Error Path:", response.error);
-        }
+        const response = yield (0, supertest_1.default)(app).post('/auth/login').send(user);
+        user.token = response.body.token;
         expect(response.statusCode).toBe(200);
         expect(response.body).toHaveProperty("token");
+    }));
+    test("Access with token permitted", () => __awaiter(void 0, void 0, void 0, function* () {
+        const response = yield (0, supertest_1.default)(app).post('/movie').set("Authorization", `Bearer ${user.token}`).send(movie1);
+        expect(response.statusCode).toBe(201);
+        expect(response.body).toHaveProperty("_id");
+    }));
+    test("Access with modified token denied", () => __awaiter(void 0, void 0, void 0, function* () {
+        const newToken = user.token + "modified";
+        const response = yield (0, supertest_1.default)(app).post('/movie').set("Authorization", `Bearer ${newToken}`).send(movie2);
+        expect(response.statusCode).toBe(401);
+    }));
+    // set jet timout to 10 seconds
+    jest.setTimeout(10000);
+    test("Token Expiration", () => __awaiter(void 0, void 0, void 0, function* () {
+        // assuming the token expiration is set to 5 second for testing purposes
+        yield new Promise(res => setTimeout(res, 6000)); // wait for 6 seconds
+        const response = yield (0, supertest_1.default)(app).post('/movie').set("Authorization", `Bearer ${user.token}`).send(movie2);
+        expect(response.statusCode).toBe(401);
     }));
 });
 //# sourceMappingURL=auth.test.js.map
--- src/tests/auth.test.ts
+++ src/tests/auth.test.ts
@@ -2,13 +2,19 @@ import request from "supertest";
 import initApp from '../index';
 import { Express } from 'express';    
 import User from "../model/userModel";
+import Movie from "../model/moviesModel";
+
 let app:Express;
 
+const user = {email: "test@example.com", password: "testpassword", _id:"", token:""};
+const movie1 = {title: "Test Movie", year: 2025};
+const movie2 = {title: "Another Test Movie", year: 2024};
 
 beforeAll(async () => {  
     console.log("Befroe All Tests") 
     app = await initApp(); 
-    await User.deleteMany();   
+    await User.deleteMany();
+    await Movie.deleteMany();   
 });
 
 afterAll(done => {      
@@ -16,22 +22,45 @@ afterAll(done => {
 })  
 
 describe('Auth API', () => {
+
+    test("Access restricted url denied", async () => {
+        const response = await request(app).post('/movie').send(movie1);
+        expect(response.statusCode).toBe(401);
+    });
+
     test('Register Test', async () => {
-        const resoponse = await request(app).post('/auth/register').send({
-            email: "test@example.com",
-            password: "testpassword"
-        });
+        const resoponse = await request(app).post('/auth/register').send(user);
+        user._id = resoponse.body._id;
         expect(resoponse.body).toHaveProperty("token");
         expect(resoponse.statusCode).toBe(201);
     });
 
     test('Login Test', async () => {
-        const response = await request(app).post('/auth/login').send({
-            email: "test@example.com",
-            password: "testpassword"
-        });
+        const response = await request(app).post('/auth/login').send(user);
+        user.token = response.body.token;
         expect(response.statusCode).toBe(200);
         expect(response.body).toHaveProperty("token");
     });
 
+    test("Access with token permitted", async () => {       
+        const response = await request(app).post('/movie').set("Authorization", `Bearer ${user.token}`).send(movie1);
+        expect(response.statusCode).toBe(201);
+        expect(response.body).toHaveProperty("_id");
+    });
+
+    test("Access with modified token denied", async () => {   
+        const newToken = user.token + "modified";    
+        const response = await request(app).post('/movie').set("Authorization", `Bearer ${newToken}`).send(movie2);
+        expect(response.statusCode).toBe(401);
+    });
+    
+    // set jet timout to 10 seconds
+    jest.setTimeout(10000);
+    
+    test("Token Expiration", async () => {
+        // assuming the token expiration is set to 5 second for testing purposes
+        await new Promise(res => setTimeout(res, 6000)); // wait for 6 seconds
+        const response = await request(app).post('/movie').set("Authorization", `Bearer ${user.token}`).send(movie2);
+        expect(response.statusCode).toBe(401);  
+    });
 });
